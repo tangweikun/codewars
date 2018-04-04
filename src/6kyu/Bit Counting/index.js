9adcb67edadd03218efacf09e7c8a11ff89a71a1
tangweikun <819573105@qq.com>
2018-04-04 16:20:07 +0800

    :sparkles: Bit Counting pro

--- src/6kyu/Bit Counting/index.js
+++ src/6kyu/Bit Counting/index.js
@@ -3,4 +3,13 @@
 //   return res ? res.length : 0
 // }
 
-export const countBits = n => n.toString(2).replace(/0/g, '').length
+// export const countBits = n => n.toString(2).replace(/0/g, '').length
+
+export const countBits = function(n) {
+  let res = 0
+  while (n > 0) {
+    res += n & 1
+    n >>= 1
+  }
+  return res
+}
