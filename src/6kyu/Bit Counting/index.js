85ef712569e6857ed703c1622fd647c47068d08f
tangweikun <819573105@qq.com>
2018-04-04 16:18:24 +0800

    :hammer: Refactor Bit Counting

--- src/6kyu/Bit Counting/index.js
+++ src/6kyu/Bit Counting/index.js
@@ -1,4 +1,6 @@
-export const countBits = n => {
-  const res = n.toString(2).match(/1/g)
-  return res ? res.length : 0
-}
+// export const countBits = n => {
+//   const res = n.toString(2).match(/1/g)
+//   return res ? res.length : 0
+// }
+
+export const countBits = n => n.toString(2).replace(/0/g, '').length
