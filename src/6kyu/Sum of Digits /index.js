4b2a7c302b29166e0a59122a43986228fd7f8461
tangweikun <819573105@qq.com>
2018-04-06 11:07:12 +0800

    :sparkles: Sum of Digits / Digital Root

--- src/6kyu/Sum of Digits /index.js
+++ src/6kyu/Sum of Digits /index.js
@@ -1,9 +1,14 @@
+// export function digital_root(n) {
+//   if (n < 10) return n
+//   let sum = 0
+//   while (n > 9) {
+//     sum += n % 10
+//     n = Math.floor(n / 10)
+//   }
+//   return digital_root(sum + n)
+// }
+
 export function digital_root(n) {
   if (n < 10) return n
-  let sum = 0
-  while (n > 9) {
-    sum += n % 10
-    n = Math.floor(n / 10)
-  }
-  return digital_root(sum + n)
+  return digital_root(n % 10 + digital_root(Math.floor(n / 10)))
 }
