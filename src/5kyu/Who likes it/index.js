e594718e3eddd426f0b9eda9178220017b47c763
tangweikun <819573105@qq.com>
2018-04-06 15:23:02 +0800

    :sparkles: Who likes it?

--- src/5kyu/Who likes it/index.js
+++ src/5kyu/Who likes it/index.js
@@ -17,18 +17,28 @@
 //   }
 // }
 
-export function likes(names) {
-  var templates = [
-    'no one likes this',
-    '{name} likes this',
-    '{name} and {name} like this',
-    '{name}, {name} and {name} like this',
-    '{name}, {name} and {n} others like this',
-  ]
-  const idx = Math.min(names.length, 4)
+// export function likes(names) {
+//   const templates = [
+//     'no one likes this',
+//     '{name} likes this',
+//     '{name} and {name} like this',
+//     '{name}, {name} and {name} like this',
+//     '{name}, {name} and {n} others like this',
+//   ]
+//   const idx = Math.min(names.length, 4)
 
-  return templates[idx].replace(
-    /{name}|{n}/g,
-    val => (val === '{name}' ? names.shift() : names.length),
-  )
+//   return templates[idx].replace(
+//     /{name}|{n}/g,
+//     val => (val === '{name}' ? names.shift() : names.length),
+//   )
+// }
+
+export function likes(names) {
+  return [
+    'no one likes this',
+    `${names[0]} likes this`,
+    `${names[0]} and ${names[1]} like this`,
+    `${names[0]}, ${names[1]} and ${names[2]} like this`,
+    `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  ][Math.min(4, names.length)]
 }
